ec6aebfac3fbdba74bf7f4b755ec47a9e55626fb
meloalright <meloalright@gmail.com>
2022-04-05 17:44:59 +0800

    + tuple

---
+++ compound_type/tuple/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("five_hundred: {}", five_hundred);
+    println!("six_point_four: {}", six_point_four);
+    println!("one: {}", one);
+
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{}' is {}.", s2, len);
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() 返回字符串的长度
+    
+
+    (s, length)
+}
